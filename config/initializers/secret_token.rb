4121a437c625a125f6e50fa2707e6e93e92574fa
Michael Muse <michaelmuse@gmail.com>
2014-01-09 10:19:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TunerApp::Application.config.secret_token = '9bbf26f9275987f605c96b3803f4c32ee415b13c75a71822522a01fe2ebcc3b9e6bbe03fce59970ac3ee4aaf61f60d69e046029704eb335edc4ecf72aea8525b'
